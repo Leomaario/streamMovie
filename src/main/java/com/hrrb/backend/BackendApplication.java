91630bb489b11e70d929d690e074a1d447633bb1
Leomaario <leomatiosaouza11@gmail.com>
2025-06-27 11:38:44 -0300

    FINALIZADO: MUDANÇA NA SENHA DO BD

--- src/main/java/com/hrrb/backend/BackendApplication.java
+++ src/main/java/com/hrrb/backend/BackendApplication.java
@@ -13,14 +13,18 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	// --- ADICIONE ESTE MÉTODO AQUI DENTRO DA CLASSE ---
-	@Bean
-	public CommandLineRunner commandLineRunner(PasswordEncoder passwordEncoder) {
-		return args -> {
+			@Bean
+			public CommandLineRunner commandLineRunner(PasswordEncoder passwordEncoder) {
+				return args -> {
+					String rawPassword = "Leo123456";
+					String encodedPassword = passwordEncoder.encode(rawPassword);
 
-			System.out.println("====================================================================");
-			System.out.println("CRUD RODANDO A MIL POR HORA !");
-			System.out.println("====================================================================");
-		};
+					System.out.println("====================================================================");
+					System.out.println("PALAVRA-PASSE CRIPTOGRAFADA GERADA PELA SUA APLICAÇÃO:");
+					System.out.println(encodedPassword);
+					System.out.println("COPIE A PALAVRA-PASSE ACIMA E ATUALIZE O BANCO DE DADOS.");
+					System.out.println("====================================================================");
+				};
+
+            };
 	}
-}
