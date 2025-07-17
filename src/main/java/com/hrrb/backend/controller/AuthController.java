21aaac5e387e092772946ae4bf807ba549d92e0d
Leomaario <leomatiosaouza11@gmail.com>
2025-07-17 11:26:23 -0300

    corrigindo bug de login, mudando url do AuthController

--- src/main/java/com/hrrb/backend/controller/AuthController.java
+++ src/main/java/com/hrrb/backend/controller/AuthController.java
@@ -27,7 +27,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "https://souzalink-coach.onrender.com/")
+@CrossOrigin(origins = "https://souzalink-coach.onrender.com")
 public class AuthController {
 
 
